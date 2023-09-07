82042c1078692d0ebd029398a1ae91a1f189def7
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2023-09-06 17:29:38 -0700

    Update exercise.cpp

--- exercise.cpp
+++ exercise.cpp
@@ -4,10 +4,5 @@
 
 int main()
 {
-    Triangle t=Triangle(Point(3,1), Point(1,2), Point(4,4));
-
-    std::cout << "Perimetro del triangulo=" << t.perimeter() << "\n";
-
-    std::cout << "Area del triangulo=" << t.area() << "\n";
-
+    return 0;
 }
